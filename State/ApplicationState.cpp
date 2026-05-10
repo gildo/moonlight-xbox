bc5af7b705b932c5e88b1afc38f9d3f4901d37e6
Jeremy Nieth <jnieth@live.de>
2026-05-10 15:14:46 +0200

    Add periodic intra refreshes to mitigate HEVCerrors

--- State/ApplicationState.cpp
+++ State/ApplicationState.cpp
@@ -60,6 +60,7 @@ Concurrency::task<void> moonlight_xbox_dx::ApplicationState::Init()
 					if (a.contains("serverAddress")) h->ServerAddress = Utils::StringFromStdString(a["serverAddress"].get<std::string>());
 					if (a.contains("macaddress")) h->MacAddress = Utils::StringFromStdString(a["macaddress"].get<std::string>());
 					else h->ComputerName = h->LastHostname;
+				    if (a.contains("idr_interval")) h->IdrInterval = a["idr_interval"];
 					this->SavedHosts->Append(h);
 				}
 			}
@@ -121,6 +122,7 @@ Concurrency::task<void> moonlight_xbox_dx::ApplicationState::UpdateFile()
 			hostJson["enable_stats"] = host->EnableStats;
 			hostJson["enable_graphs"] = host->EnableGraphs;
 			hostJson["serverAddress"] = Utils::PlatformStringToStdString(host->ServerAddress);
+			hostJson["idr_interval"] = host->IdrInterval;
 
 			std::string macAddr = Utils::PlatformStringToStdString(host->MacAddress);
 			if (macAddr != "00:00:00:00:00:00" && macAddr != "")
